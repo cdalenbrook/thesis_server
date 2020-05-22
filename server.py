efcd4ed38f9acd081f47f8166fef57d52575109e
Charlotte Dalenbrook <c.dalenbrook@gmail.com>
2020-05-22 15:09:37 +0200

    reqs

--- server.py
+++ server.py
@@ -2,8 +2,10 @@ import click
 from flask import Flask, request
 import decisiontree
 from utils import store_tree, get_tree, get_target
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/make-tree', methods=['POST'])
