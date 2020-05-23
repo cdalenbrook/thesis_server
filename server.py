98e20a54d6983e9cdf49c312d9ec41e846608904
Charlotte Dalenbrook <c.dalenbrook@gmail.com>
2020-05-23 15:48:36 +0200

    gp

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import click
 from flask import Flask, request
 import decisiontree
-from utils import store_tree, get_tree, get_target
+import utils as utilities
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,24 +11,31 @@ CORS(app)
 @app.route('/make-tree', methods=['POST'])
 def handle_data():
     try:
+        user_id = request.args.get('id', 'default')
         result = decisiontree.main(
             request.json['category'], request.json['data'])
 
-        store_tree(result, request.args.get(
-            'id', ''), request.json['category'])
+        utilities.store_tree(user_id, result)
+        utilities.store_targets(user_id, request.json['category'])
+        utilities.store_usertruth(user_id, request.json['data'])
         return 'success'
     except Exception as err:
         print(err)
-        return 'failed'
+        return 'failed', 500
 
 
 @app.route('/predict-category', methods=['POST'])
 def predict():
-    tree_id = request.args.get('id', '')
-    tree = get_tree(tree_id)
-    target = get_target(tree_id)
-    if(tree is None):
-        return 'Your tree doesn\'t exist yet!'
-    y_pred = decisiontree.predict(
-        tree, request.json['toy_id'], request.json['user_truth'], target)
-    return {'prediction': y_pred.tolist()}
+    try:
+        tree_id = request.args.get('id', 'default')
+        tree = utilities.get_tree(tree_id)
+        target = utilities.get_target(tree_id)
+        user_truth = utilities.get_usertruth(tree_id)
+        if(tree is None or target is None or user_truth is None):
+            return 'Your tree doesn\'t exist yet!', 404
+        y_pred = decisiontree.predict(
+            tree, request.json['toy_id'], user_truth, target)
+        return {'prediction': y_pred.tolist()}
+    except Exception as err:
+        print(err)
+        return 'Failed', 500
--- utils.py
+++ utils.py
@@ -3,11 +3,19 @@ from typing import List
 
 all_trees = {}
 targets = {}
+user_truths = {}
 
 
-def store_tree(tree: DecisionTreeClassifier, id: str, target: List[str]):
+def store_tree(id: str, tree: DecisionTreeClassifier):
     all_trees[id] = tree
+
+
+def store_targets(id: str, target: List[str]):
     targets[id] = target[0]
+
+
+def store_usertruth(id: str, data):
+    user_truths[id] = data
 
 
 def get_tree(id: str):
@@ -19,4 +27,10 @@ def get_tree(id: str):
 def get_target(id: str):
     if(id in targets):
         return targets[id]
+    return None
+
+
+def get_usertruth(id: str):
+    if(id in user_truths):
+        return user_truths[id]
     return None
