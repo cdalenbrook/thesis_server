9261df76d34697e5a7c551c15e9adb47a164ead2
Charlotte Dalenbrook <c.dalenbrook@gmail.com>
2020-06-24 13:54:08 +0200

    GA's

--- ga2.py
+++ ga2.py
@@ -36,6 +36,7 @@ class FeatureSelectionGA():
         # make last column contain the class values
         df = df[[c for c in df if c not in ['outside']]
                 + [c for c in ['outside'] if c in df]]
+        print('preprocessed DF', df)
         return df
 
     def preprocess_cars(self, df: pd.DataFrame):
@@ -67,11 +68,11 @@ class FeatureSelectionGA():
     def fit_trees(self, population: list, df: pd.DataFrame, target: str):
         # run the DT algorithm with the subset of features in the population
         # X contains columns in the population
-        accuracies = [0] * len(population)
-        depths = [0] * len(population)
-        trees = [0] * len(population)
-        num_features = [0] * len(population)
-        i = 0
+        accuracies = []
+        depths = []
+        trees = []
+        num_features = []
+
         # loop through genotypes in population and fit a tree to the given features and then find accuracy & depth
         for pop in population:
             clf = DecisionTreeClassifier(criterion="entropy", random_state=100)
@@ -84,21 +85,35 @@ class FeatureSelectionGA():
                 count += 1
             # remove the target column from the X values
             X = X.drop(target, 1)
+            # if there are no features in the final column, skip this population
             if X.empty:
-                accuracies[i] = 0
-                depths[i] = 0
-                trees[i] = None
-                num_features[i] = 0
-                i += 1
+                accuracies.append(0)
+                depths.apppend(0)
+                trees.append(None)
+                num_features.append(0)
+                print('No Tree Made')
                 continue
             # make the target column the y values
             y = df.iloc[:, -1]
 
             X_train, X_test, y_train, y_test = train_test_split(
                 X, y, test_size=0.2, random_state=100)
-
             # fit the tree to the given training values
             clf = clf.fit(X_train, y_train)
+
+            # take the features out of the population that are not used in the tree
+            feature_importances = clf.feature_importances_
+            change = []
+            for j in range(len(feature_importances)):
+                if feature_importances[j] == 0:
+                    change.append(j)
+            count = 0
+            for k in range(len(pop)):
+                if pop[k] == 1:
+                    if count in change:
+                        pop[k] = 0
+                    count += 1
+
             # make predictions according to the fitted tree
             y_pred = clf.predict(X_test)
             # get the accuracy of the DT for current population
@@ -106,13 +121,12 @@ class FeatureSelectionGA():
             # get the depth of the DT for current pop
             tree_depth = clf.tree_.max_depth
             # add both values to their respective arrays
-            accuracies[i] = pop_accuracy
-            depths[i] = tree_depth
-            # add the tree to the array (later used for visualisation of the fittest tree)
-            trees[i] = clf
+            accuracies.append(pop_accuracy)
+            depths.append(tree_depth)
+            trees.append(clf)
             # add the number of features used in training to the list of num features
-            num_features[i] = len(X.columns)
-            i += 1
+            num_features.append(len(X.columns))
+
         return accuracies, depths, trees, num_features
 
     def get_fitness(self, accuracies: list, depths: list, num_features: list):
@@ -195,24 +209,27 @@ class FeatureSelectionGA():
             pop = new_population
 
         print('Final Population: \n', pop)
-        return pop, trees, df, fitnesses
+        print(trees)
+        return pop, trees, df, fitnesses, accuracies, depths
 
-    def display_optimized_pop(self, pop: list, trees: list, df: pd.DataFrame, fitnesses: list):
+    def display_optimized_pop(self, pop: list, trees: list, df: pd.DataFrame, fitnesses: list, accuracies: list, depths: list):
         fittest_idx = fitnesses.index(max(fitnesses))
         fittest = pop[fittest_idx]
         print(fittest)
+
         features = list(df.columns)
         best_features = []
         for i in range(len(fittest)):
             if(fittest[i] == 1):
                 best_features.append(features[i])
+
         print('Most Important Features', best_features)
 
-        print(
-            dict(zip(df[best_features], trees[fittest_idx].feature_importances_)))
+        print(trees[fittest_idx].feature_importances_)
+        print('Accuracy: ', accuracies[fittest_idx])
+        print('Depth: ', depths[fittest_idx])
 
         clf = trees[fittest_idx]
-        print(clf)
         fn = best_features
         cn = ['inside', 'outside']
         fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(25, 10), dpi=300)
@@ -225,5 +242,5 @@ path = "./data_categorized.csv"
 df = pd.read_csv(path)
 ga = FeatureSelectionGA(df, df_type='toys', crossover_prob=0.6,
                         mutation_prob=0.2, tournament_size=6, num_gens=10)
-pop, trees, df, fitnesses = ga.optimize()
-ga.display_optimized_pop(pop, trees, df, fitnesses)
+pop, trees, df, fitnesses, accuracies, depths = ga.optimize()
+ga.display_optimized_pop(pop, trees, df, fitnesses, accuracies, depths)
